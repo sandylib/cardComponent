e3da4271cdedc7306cf1c66d8c812107c11c7696
Sandy <sandyz@bizcover.com.au>
2017-06-05 10:26:36 +1000

    move styles to component

--- src/App.js
+++ src/App.js
@@ -7,16 +7,41 @@ import BackgroundImage from 'react-background-image-loader';
 const Logo = styled.img`
     height:75px;
 `;
+
+
+
+
+ const Mask = styled.ul`
+    width:100%;  
+     display:table-row;    
+     position: absolute;    
+     bottom: 0;
+    list-style-type: none;
+    background: rgba(0, 0,0, 0.6);
+    padding:0;
+    margin: 0;
+    vertical-align: center;
+ `;
+
+  const Item = styled.li`
+    display: table-cell;
+    list-style-type: none;
+    vertical-align: middle;
+    font-size: 25px;
+    color: white;
+    line-height: 18px;
+ `;
+
 class App extends Component {
   render() {
     return (
       <BackgroundImage src={require('./assets/tile.jpg')}  className="composite">  
-          <div className="footer">
-              <ul>
-                  <li><Logo src={require('./assets/logo.png')} className="logo" alt="logo" /></li>
-                  <li>Home And Away</li>
-              </ul>
-          </div>
+          
+              <Mask>             
+                  <Item><Logo src={require('./assets/logo.png')} className="logo" alt="logo" /></Item>
+                  <Item>Home And Away</Item>
+             </Mask>
+          
         </BackgroundImage>
     );
   }
